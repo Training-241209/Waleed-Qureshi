17444c4d5dd259738953c9d103bb857747e37c6e
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2024-12-23 00:41:04 -0500

    register improved, added login, ticket form

--- Project1-ERS/ers-frontend/src/App.tsx
+++ Project1-ERS/ers-frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { AddTicketForm } from './features/auth/components/add-ticket-form'
+import { LoginForm } from './features/auth/components/login-form'
 import { RegisterForm } from './features/auth/components/register-form'
 
 function App() {
@@ -6,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <RegisterForm/>
+        <AddTicketForm/>
       </div>
     </>
   )
